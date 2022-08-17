152fff1b9e39d14b6eaa5d83da9d67b6d58e375a
kbrother <00076289@kaist.ac.kr>
2022-08-17 13:29:06 +0900

    execute one by one

--- main.m
+++ main.m
@@ -1,75 +1,42 @@
-%  ~/MATLAB/R2022a/bin/matlab -nodisplay -nodesktop -r "run('main.m');"
+% ~/MATLAB/R2022a/bin/matlab -nodisplay -nodesktop -r "main SFAS91";
 % ps -A | grep MATLAB | awk '{print $1}' | xargs kill -9 $1
 % mosquitto_sub -h 143.248.221.190 -u kaist_fire -P 'KaistFire!123' -t /PRE_S/SFAS91 > sfas91_ours_0613.txt
+function main(topic)
 addpath(genpath('lib'));
 addpath(genpath('sofia'));
 addpath(genpath('sofia/mylib'));
 addpath(genpath('MQTT'))
 warning('off')
+disp(topic);
 %javaaddpath('MQTT/jar/org.eclipse.paho.client.mqttv3-1.1.0.jar')
 %javaaddpath('MQTT/mqttasync.jar')
 
-declare_tensor();
+init_vars();
 brokerAddress = 'tcp://143.248.221.190';
 port = 1883;
-connection(brokerAddress, port);
+connection(topic, brokerAddress, port);
+end
 
 % function for subscribe
-function connection(brokerAddress, port)
+function connection(topic, brokerAddress, port)
 	global mqClient
 	mqClient = mqttclient(brokerAddress, Port=port, Username="kaist_fire", Password="KaistFire!123");
-	mysub = subscribe(mqClient, '/CFD/#', Callback=@cb);
+	mysub = subscribe(mqClient, strcat('/CFD/', topic), Callback=@cb);
 	disp(mysub);
 end
 
 % function that initialize topic list
-function declare_tensor
-	global init_tensor
-	global init_tensor_mean
-	global init_tensor_std
-	global weighted_sum
-	global weighted_sq_sum
-	global X_last
-	global U
-	global O
-	global L
-	global B
-	global F
-	global sigma
-	global output_json
-	global Yt
-	global W
-	global real_num_id
+function init_vars
 	global alpha 
-	global alpha_sum
-	global sfas53_cnt
+	global alpha_sum	
 	global cnt
 
-	sfas53_cnt = 0;
+	cnt = 0;
 	alpha = 0.6;
-	%alpha_sum = 0;	
-	alpha_sum = struct;
-	init_tensor = struct;
-	init_tensor_mean = struct;
-	init_tensor_std = struct;
-	tensor_window = struct;
-	weighted_sum = struct;
-	weighted_sq_sum = struct;
-	init_tensor_mean = struct;
-	U = struct;
-	O = struct;
-	L = struct;
-	B = struct;
-	F = struct;
-	ouput_json = struct;
-	sigma = struct;
-	Yt = struct;
-	W = struct;
-	real_num_id = struct;	
-	cnt = struct;
+	alpha_sum = 0;			
 end
 
-function initialize_tensor(topic, msg, num_channel, window_len)
+function initialize_tensor(msg, num_channel, window_len)
 	% Create tensors
 	global X_last
 	global init_tensor	
@@ -86,45 +53,43 @@ function initialize_tensor(topic, msg, num_channel, window_len)
 	global F
 	global real_num_id	
 
-	fprintf('number of channel in this topic: %d\n', num_channel);
-	if ~isfield(init_tensor, topic)
-		init_tensor.(topic) = cell(num_channel, 1);
-		init_tensor_mean.(topic) = cell(num_channel, 1);
-		init_tensor_std.(topic) = cell(num_channel, 1);
-		window_sum.(topic) = cell(num_channel, 1);
-		window_sq_sum.(topic) = cell(num_channel, 1);
-		U.(topic) = cell(num_channel, 2);		
-		sigma.(topic) = cell(num_channel, 1);
-		Yt.(topic) = cell(num_channel, 1);
-		W.(topic) = cell(num_channel, 1);
-		L.(topic) = cell(num_channel, 1);
-		B.(topic) = cell(num_channel, 1);
-		F.(topic) = cell(num_channel, 1);
-		X_last.(topic) = cell(num_channel, 1);
-		real_num_id.(topic) = cell(num_channel, 1);
-	end 					
+	fprintf('number of channel in this topic: %d\n', num_channel);	
+	init_tensor = cell(num_channel, 1);
+	init_tensor_mean = cell(num_channel, 1);
+	init_tensor_std = cell(num_channel, 1);
+	window_sum = cell(num_channel, 1);
+	window_sq_sum = cell(num_channel, 1);
+	U = cell(num_channel, 2);		
+	sigma = cell(num_channel, 1);
+	Yt = cell(num_channel, 1);
+	W = cell(num_channel, 1);
+	L = cell(num_channel, 1);
+	B = cell(num_channel, 1);
+	F = cell(num_channel, 1);
+	X_last = cell(num_channel, 1);
+	real_num_id = cell(num_channel, 1);	
 			
 	for i=1:num_channel										
 		num_id = max(msg(i).id);
-		real_num_id.(topic){i} = 0;
+		real_num_id{i} = 0;
 		% fprintf("num id: %d\n", num_id);
 		for j=1:num_id			
 			if msg(i).err(j) == 0
-				real_num_id.(topic){i} = real_num_id.(topic){i} + 1;
+				real_num_id{i} = real_num_id{i} + 1;
 			end
 		end
 
-		if real_num_id.(topic){i} > 0
-			init_tensor.(topic){i} = tenzeros([real_num_id.(topic){i} 7 window_len]);
-			init_tensor_mean.(topic){i} = tenzeros([real_num_id.(topic){i} 7 window_len]);
-			init_tensor_std.(topic){i} = tenzeros([real_num_id.(topic){i} 7 window_len]);	
-			weighted_sum.(topic){i} = zeros(real_num_id.(topic){i}, 7);
-			weighted_sq_sum.(topic){i} = zeros(real_num_id.(topic){i}, 7);					
+		if real_num_id{i} > 0
+			init_tensor{i} = tenzeros([real_num_id{i} 7 window_len]);
+			init_tensor_mean{i} = tenzeros([real_num_id{i} 7 window_len]);
+			init_tensor_std{i} = tenzeros([real_num_id{i} 7 window_len]);	
+			weighted_sum{i} = zeros(real_num_id{i}, 7);
+			weighted_sq_sum{i} = zeros(real_num_id{i}, 7);					
 		end	
 	end 		
 end
 
-function fill_tensor(topic, msg, num_channel, time_stamp, window_len)
+function fill_tensor(msg, num_channel, time_stamp, window_len)
 	global X_last
 	global init_tensor			
 	global init_tensor_mean
@@ -134,27 +99,27 @@ function fill_tensor(topic, msg, num_channel, time_stamp, window_len)
 	global weighted_sum
 	global weighted_sq_sum
 	global real_num_id
-	
-	alpha_sum.(topic) = alpha*alpha_sum.(topic) + 1;
+
+	alpha_sum = alpha*alpha_sum + 1;
 	for i=1:num_channel				
 		num_id = max(msg(i).id);
-		if real_num_id.(topic){i} == 0
+		if real_num_id{i} == 0
 			continue
 		end						
 
-		curr_matrix = X_last.(topic){i};				
-		weighted_sum.(topic){i} = alpha * weighted_sum.(topic){i} + curr_matrix;					
-		weighted_sq_sum.(topic){i} = alpha * weighted_sq_sum.(topic){i} + curr_matrix .* curr_matrix;	
+		curr_matrix = X_last{i};				
+		weighted_sum{i} = alpha * weighted_sum{i} + curr_matrix;					
+		weighted_sq_sum{i} = alpha * weighted_sq_sum{i} + curr_matrix .* curr_matrix;	
 
-		init_tensor.(topic){i}(:, :, time_stamp) = X_last.(topic){i};					
-		curr_mean = weighted_sum.(topic){i} / alpha_sum.(topic);
-		curr_std = sqrt(weighted_sq_sum.(topic){i} / alpha_sum.(topic) - curr_mean .* curr_mean);				
-		init_tensor_mean.(topic){i}(:, :, time_stamp) = curr_mean;
-		init_tensor_std.(topic){i}(:, :, time_stamp) = curr_std;									
+		init_tensor{i}(:, :, time_stamp) = X_last{i};					
+		curr_mean = weighted_sum{i} / alpha_sum;
+		curr_std = sqrt(weighted_sq_sum{i} / alpha_sum - curr_mean .* curr_mean);				
+		init_tensor_mean{i}(:, :, time_stamp) = curr_mean;
+		init_tensor_std{i}(:, :, time_stamp) = curr_std;									
 	end	
 end
 
-function parse_traffic(topic, msg, num_channel)	
+function parse_traffic(msg, num_channel)	
 	global X_last;
 	global init_tensor				
 	global alpha	
@@ -163,10 +128,10 @@ function parse_traffic(topic, msg, num_channel)
 	global real_num_id
 
 	for i=1:num_channel	
-		if real_num_id.(topic){i} > 0				
+		if real_num_id{i} > 0				
 			idx_cnt = 1;			
 			num_id = max(msg(i).id);										
-			curr_matrix = zeros(real_num_id.(topic){i}, 7);									
+			curr_matrix = zeros(real_num_id{i}, 7);									
 			for j=1:num_id
 				if msg(i).err(j) == 0
 					curr_matrix(idx_cnt, 1) = msg(i).temp(j);
@@ -179,30 +144,30 @@ function parse_traffic(topic, msg, num_channel)
 					idx_cnt = idx_cnt + 1;
 				end
 			end	
-			X_last.(topic){i} = curr_matrix;																			
+			X_last{i} = curr_matrix;																			
 		end				
 	end	
 end
 
-function normalize_init_tensor(msg, topic, num_channel)
+function normalize_init_tensor(msg, num_channel)
 	global init_tensor
 	global init_tensor_mean
 	global init_tensor_std
 	global real_num_id
 
 	for i=1:num_channel				
-		if real_num_id.(topic){i} > 0		
-			curr_mask = find(init_tensor_std.(topic){i} < 1e-3);
+		if real_num_id{i} > 0		
+			curr_mask = find(init_tensor_std{i} < 1e-3);
 			if ~isempty(curr_mask)
-				init_tensor_std.(topic){i}(curr_mask) = 1e-3;			
+				init_tensor_std{i}(curr_mask) = 1e-3;			
 			end
-			init_tensor.(topic){i} = init_tensor.(topic){i} - init_tensor_mean.(topic){i};		
-			init_tensor.(topic){i} = init_tensor.(topic){i} ./ init_tensor_std.(topic){i};		
+			init_tensor{i} = init_tensor{i} - init_tensor_mean{i};		
+			init_tensor{i} = init_tensor{i} ./ init_tensor_std{i};		
 		end		
 	end	
 end
 
-function normalize_matrix(msg, topic, num_channel)
+function normalize_matrix(msg, num_channel)
 	global tensor_window
 	global Yt
 	global weighted_sum
@@ -212,18 +177,18 @@ function normalize_matrix(msg, topic, num_channel)
 	global X_last
 
 	for i=1:num_channel		
-		if real_num_id.(topic){i} > 0 				
-			avg_mat = weighted_sum.(topic){i} / alpha_sum.(topic); 					
-			std_mat = sqrt(weighted_sq_sum.(topic){i} / alpha_sum.(topic) - avg_mat .* avg_mat);					
+		if real_num_id{i} > 0 				
+			avg_mat = weighted_sum{i} / alpha_sum; 					
+			std_mat = sqrt(weighted_sq_sum{i} / alpha_sum - avg_mat .* avg_mat);					
 			std_mat(std_mat < 1e-3) = 1e-3;
 			
-			Yt.(topic){i} = X_last.(topic){i} - avg_mat;
-			Yt.(topic){i} = Yt.(topic){i} ./ std_mat;					
+			Yt{i} = X_last{i} - avg_mat;
+			Yt{i} = Yt{i} ./ std_mat;					
 		end		
 	end
 end
 
-function denorm_Xhat = denormalize_matrix(Xhat, topic, channel_id, outlier)
+function denorm_Xhat = denormalize_matrix(Xhat, channel_id, outlier)
 	global tensor_window
 	global Yt
 	global weighted_sum
@@ -233,21 +198,17 @@ function denorm_Xhat = denormalize_matrix(Xhat, topic, channel_id, outlier)
 	global alpha
 	global X_last;
 
-	assert(real_num_id.(topic){channel_id} > 0);
-	avg_mat = weighted_sum.(topic){channel_id} / alpha_sum.(topic);
-	std_mat = sqrt(weighted_sq_sum.(topic){channel_id} / alpha_sum.(topic) - avg_mat .* avg_mat);
+	assert(real_num_id{channel_id} > 0);
+	avg_mat = weighted_sum{channel_id} / alpha_sum;
+	std_mat = sqrt(weighted_sq_sum{channel_id} / alpha_sum - avg_mat .* avg_mat);
 	std_mat(std_mat < 1e-3) = 1e-3;
 
 	denorm_O = outlier .* std_mat;
 	denorm_O(abs(denorm_O) < 500) = 0;
 	denorm_Xhat = Xhat .* std_mat + avg_mat;	
 
-	weighted_sum.(topic){channel_id} = alpha * weighted_sum.(topic){channel_id} + (X_last.(topic){channel_id} - denorm_O);
-	weighted_sq_sum.(topic){channel_id} = alpha * weighted_sq_sum.(topic){channel_id} + (X_last.(topic){channel_id}-denorm_O) .* (X_last.(topic){channel_id}-denorm_O);	
-end
-
-function test_cb(topic, data)
-	jsondecode(data);
+	weighted_sum{channel_id} = alpha * weighted_sum{channel_id} + (X_last{channel_id} - denorm_O);
+	weighted_sq_sum{channel_id} = alpha * weighted_sq_sum{channel_id} + (X_last{channel_id}-denorm_O) .* (X_last{channel_id}-denorm_O);	
 end
 
 function cb(topic, data)
@@ -274,6 +235,7 @@ function cb(topic, data)
 	mu = 0.01;
 	window_len = 100;	
 	rank = 20;
+	period = 43200;
 
 	% temp hum pm1 pm2 pm10 co2 co
 	%fireMQTT = mqtt('tcp://143.248.221.190', 'Username', 'kaist_fire', 'Port', 1883, 'Password', "KaistFire!123");    		
@@ -281,109 +243,102 @@ function cb(topic, data)
     topic = split(topic, "/");
 	topic = topic{3};
 
-	% Intialize count	
-	if ~isfield(cnt, topic)
-		cnt.(topic) = 1;
-	else
-		cnt.(topic) = cnt.(topic) + 1;
+	% Update count
+	if cnt <= window_len
+		cnt = cnt + 1;
 	end
-
-	% Intialize / update alpha	
-	if cnt.(topic) == 1
-		alpha_sum.(topic) = 0;			
-	end	
 		
 	% Initialize tensor
 	num_channel = size(jsondata.cfd, 1);
-    if cnt.(topic) == 1		
-		initialize_tensor(topic, jsondata.cfd, num_channel, window_len);	   
+    if cnt == 1		
+		initialize_tensor(jsondata.cfd, num_channel, window_len);	   
 		disp("init finish");
     end
 
 	% Fill init tensor	
-	parse_traffic(topic, jsondata.cfd, num_channel);		
-	if cnt.(topic) <= window_len							
-		fill_tensor(topic, jsondata.cfd, num_channel, cnt.(topic), window_len);		
+	parse_traffic(jsondata.cfd, num_channel);		
+	if cnt <= window_len							
+		fill_tensor(jsondata.cfd, num_channel, cnt, window_len);		
 		disp("fill finish");
 	end
 	
 	% Intialize sofia
-	if cnt.(topic) == window_len
+	if cnt == window_len
 		% Normalize tensor
-		normalize_init_tensor(jsondata.cfd, topic, num_channel);				
+		normalize_init_tensor(jsondata.cfd, num_channel);				
 		for i=1:num_channel		
 			% Initialization
-			if real_num_id.(topic){i} > 0
-				omega_temp = logical(double(tenones([real_num_id.(topic){i} 7 window_len])));
-				%disp(init_tensor.(topic){i});					
-				[U_init, X_hat.(topic), O.(topic), ~] = sofia_init(init_tensor.(topic){i}, omega_temp, rank, lambda1, lambda3);							
-				U.(topic){i, 1} = U_init{1};											
-				U.(topic){i, 2} = U_init{2};				
+			if real_num_id{i} > 0
+				omega_temp = logical(double(tenones([real_num_id{i} 7 window_len])));		
+				%disp(init_tensor{i});						
+				[U_init, X_hat, O, ~] = sofia_init(init_tensor{i}, omega_temp, rank, lambda1, lambda3);							
+				U{i, 1} = U_init{1};											
+				U{i, 2} = U_init{2};				
 				W_init = U_init{3};						
 				for n=1:2
-					weights = sqrt(sum(U.(topic){i, n}.^2, 1));
-					U.(topic){i, n} = U.(topic){i, n} ./ weights;
+					weights = sqrt(sum(U{i, n}.^2, 1));
+					U{i, n} = U{i, n} ./ weights;
 					W_init = W_init .* weights;
 				end;
-				W.(topic){i} = W_init(end, :);				
+				W{i} = W_init(end, :);				
 
 				% HW fitting		
 				%disp(W_init);
-				[~,L.(topic){i},B.(topic){i},F.(topic){i}] = hw_add_add_fit(W_init);								
+				[~,L{i},B{i},F{i}] = hw_add_add_fit(W_init);								
 
 				% Initialize error scale tensor
-				Ysz = size(init_tensor.(topic){i});
-				sigma.(topic){i} = 0.1*ones([Ysz(1), Ysz(2)]);										
+				Ysz = size(init_tensor{i});
+				sigma{i} = 0.1*ones([Ysz(1), Ysz(2)]);										
 			end
 		end		
 		disp("sofia init finish");
 	end	
 
 	% Dynamic updates	
-	if cnt.(topic) > window_len				
-		normalize_matrix(jsondata.cfd, topic, num_channel);						
+	if cnt > window_len				
+		normalize_matrix(jsondata.cfd, num_channel);						
 		output_json.pre = cell(num_channel, 1);
 		for i=1:num_channel		
 			num_id = max(jsondata.cfd(i).id);
 			curr_output.ch = i;	
 
-			if real_num_id.(topic){i} > 0				
-				Omega_temp = ones(real_num_id.(topic){i}, 7);
-				U_N = hw_add_add_forecast(L.(topic){i}, B.(topic){i}, 1);						
-				Ythat = U.(topic){i, 1} * diag(U_N) * U.(topic){i, 2}';								
+			if real_num_id{i} > 0				
+				Omega_temp = ones(real_num_id{i}, 7);
+				U_N = hw_add_add_forecast(L{i}, B{i}, 1);						
+				Ythat = U{i, 1} * diag(U_N) * U{i, 2}';								
 
-				Rt = Yt.(topic){i} - Ythat;								
-				cRt = huber(Rt./sigma.(topic){i}) .*sigma.(topic){i};								
-				sigma.(topic){i} = sigma_update(sigma.(topic){i}, Rt, Omega_temp, 0.01);
+				Rt = Yt{i} - Ythat;								
+				cRt = huber(Rt./sigma{i}) .*sigma{i};								
+				sigma{i} = sigma_update(sigma{i}, Rt, Omega_temp, 0.01);
 										
 				cRt = Omega_temp .* cRt;			
 				G = cell(3, 1);
 				
-				G{1} = cRt * U.(topic){i, 2} * diag(U_N);
-				G{2} = cRt' * U.(topic){i, 1} * diag(U_N);													
-				G{3} = (khatrirao(U.(topic){i, 1},U.(topic){i, 2})' * reshape(cRt,[],1))' + lambda1 * (W.(topic){i} - U_N);
+				G{1} = cRt * U{i, 2} * diag(U_N);
+				G{2} = cRt' * U{i, 1} * diag(U_N);													
+				G{3} = (khatrirao(U{i, 1},U{i, 2})' * reshape(cRt,[],1))' + lambda1 * (W{i} - U_N);
 						
 				for n=1:2
 					G{n} = G{n} * min(1, mu * sqrt(rank)/norm(G{n}, 'fro')); % What?
-					U.(topic){i, n} = U.(topic){i, n} + mu * G{n};
+					U{i, n} = U{i, n} + mu * G{n};
 				end					
 
 				G{3} = G{3} * min(1, mu * sqrt(rank)/norm(G{3}, 'fro')); % What?
 				U_N = U_N + mu * G{3};					
 
 				for n=1:2
-					weights = sqrt(sum(U.(topic){i, n}.^2, 1));
-					U.(topic){i, n} = U.(topic){i, n} ./ weights;
+					weights = sqrt(sum(U{i, n}.^2, 1));
+					U{i, n} = U{i, n} ./ weights;
 					U_N = U_N .* weights;
 				end
 							
-				[L.(topic){i}, B.(topic){i}] = hw_add_add_update(U_N, L.(topic){i}, B.(topic){i}, F.(topic){i});				
+				[L{i}, B{i}] = hw_add_add_update(U_N, L{i}, B{i}, F{i});				
 				%disp("hw add add update");
-				W.(topic){i} = U_N;
+				W{i} = U_N;
 
-				X_hat = double(full(ktensor({U.(topic){i, 1}, U.(topic){i, 2}, U_N})));
-				outlier = Yt.(topic){i} - (Ythat + cRt);			
-				denorm_X_hat = denormalize_matrix(X_hat, topic, i, outlier);								
+				X_hat = double(full(ktensor({U{i, 1}, U{i, 2}, U_N})));
+				outlier = Yt{i} - (Ythat + cRt);			
+				denorm_X_hat = denormalize_matrix(X_hat, i, outlier);								
 				denorm_X_hat(denorm_X_hat < 0) = 1e-3;
 				idx_cnt = 1;
 
@@ -450,9 +405,9 @@ function cb(topic, data)
 			end	
 		end				
 		output_json = jsonencode(output_json);					
-		write(mqClient, strcat('/PRE_S/', topic), output_json);				
-		alpha_sum.(topic) = alpha*alpha_sum.(topic) + 1;
-	end			
+		write(mqClient, strcat('/PRE_S/', topic), output_json);						
+		alpha_sum = alpha*alpha_sum + 1;
+	end				
 end
 
 
